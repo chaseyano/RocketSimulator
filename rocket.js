d7aaec128d906b41121b9570b8d5347ed630e5a2
Chase Yano <chase.yano@columbia.edu>
2023-11-14 20:15:05 -0500

    added sris changed from doc

--- rocket.js
+++ rocket.js
@@ -1,12 +1,15 @@
+
 class Rocket {
     constructor(canvasId, material, fuelType) {
         this.canvas = document.getElementById(canvasId);
         this.direction = 1; // -1 for up, 1 for down
         this.isAnimating = false;
             console.log(this.canvas);
-
+ 
+ 
         paper.setup(this.canvas);
-
+ 
+ 
         this.rocketShape = new paper.Path.Rectangle({
             point: [this.canvas.width / 2 - 50, this.canvas.height - 150], // Positioned at the bottom and centered horizontally
             size: [50, 100],
@@ -15,27 +18,40 @@ class Rocket {
         this.rocketWeight = 0.0;
         if (material === "titanium") {
             this.rocketWeight = 100.0;
+        } else if (material === "aluminium") {
+            this.rocketWeight = 50.0;
+        } else if (material === "steel") {
+            this.rocketWeight = 150.0;
         }
-
+ 
+ 
         this.force = 0.0;
         this.fuelWeight = 0.0;
         if (fuelType === "hydrogen") {
             this.force = 50;
             this.fuelWeight = 50.0;
+        } else if (fuelType === "kerosene") {
+            this.force = 40;
+            this.fuelWeight = 60.0;
+        } else if (fuelType === "methane") {
+            this.force = 45;
+            this.fuelWeight = 55.0;
         }
-
+ 
+ 
         this.weightLossConstant = 0.0;
-        if (fuelType === "hydrogen" && material === "titanium") {
+        if ((fuelType === "hydrogen" && material === "titanium") || (fuelType === "kerosene" && material === "aluminium") || (fuelType === "methane" && material === "steel")) {
             this.weightLossConstant = 5.5;
         }
-        this.speed = 0.0;
     }
-
+ 
+ 
     animate() {
         if (!this.isAnimating) {
             return;
         }
-
+ 
+ 
         //     // real rocket shouldn't bounce
         // if (this.rocketShape.position.y - this.rocketShape.bounds.height / 2 <= 0) {
         //     this.direction = 1; // Change direction to down
@@ -44,7 +60,8 @@ class Rocket {
         // }
         console.log("rocket shape position y -> " + String(this.rocketShape.position.y) );
         console.log("this.canvas.height - 150 -> " + String(this.canvas.height - 150 ));
-
+ 
+ 
         this.getNewSpeed();
         if (this.rocketShape.position.y >= this.canvas.height - 150) {
             console.log("position is above ground");
@@ -52,76 +69,54 @@ class Rocket {
         }
         paper.view.update(); // Update the view
         requestAnimationFrame(this.animate.bind(this)); // Bind 'this' to ensure correct context
-
+ 
+ 
     }
-
+ 
+ 
     startAnimation() {
         if (!this.isAnimating) {
             this.isAnimating = true;
             this.animate();
         }
     }
-
+ 
+ 
     stopAnimation() {
         this.isAnimating = false;
     }
-
+ 
+ 
     launch() {
         this.startAnimation();
     }
-
-
+ 
+ 
+ 
+ 
     loseWeight() {
         this.fuelWeight -= this.weightLossConstant;
     }
-
+ 
+ 
     getGravityForce() {
         return -(this.rocketWeight + this.fuelWeight) * 9.8;
     }
-
+ 
+ 
     calculateAcceleration() {
         if (this.fuelWeight >= 0) {
             return (this.force + this.getGravityForce()) / (this.rocketWeight + this.fuelWeight);
         } else {
-            return 
+            return
         }
     }
-
+ 
+ 
     getNewSpeed() {
         this.speed += this.calculateAcceleration();
     }
-}
+ }
 
-
-// FUNCTIONS?
-
-// launch()
-    // trigger the flying
-    // set a threshold for leaving atmosphere in terms of 
-    // displacement and velocity
-    // if threshold is met while accelration is 0 or positive -> escapeAtmosphere()
-    // we know it fails if its acceleration becomes negative -> failEscpae()
-
-// escapeAtmosphere() SUCCESS CONDITION, WIN
-    // if you reach teh correct position with the correct velocity, 
-    // trigger a static animation that shows the ascent into space
-    // YOU WIN
-// failEscape()
-    // if by that same time, you are NOT where you need
-    // animate the rocket crashing YOU LOSE
-    // TODO: hints?
-
-// fly()
-    // make the weight change of the rocket
-    // determine the force exerted in that moment by the rockets -> convert from force to accel with calculateAcceleration()
-    // these are fields of the class (accel and weight)
-
-// calculateAcceleration()
-    // this is the main driver
-    // takes into account the user stuff
-    // with acceleration, we know how to increment the speed?
-    // needs to take into account (weight (fuel is burned constantly), )
-// also needed -> how does the constructor take different values?
-// how do different properties of the rocket change the fly() function
-
-export default Rocket;
+ 
+export default Rocket; 
